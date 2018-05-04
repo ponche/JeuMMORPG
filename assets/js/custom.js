d2a3621905a08ef5b5b537dcc346916a7dc8b59d
Antoine Bouquet <35431329+BouquetAntoine@users.noreply.github.com>
2018-05-04 16:48:00 +0200

    Removing stars

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -59,71 +59,9 @@ function getMousePos(canvas, evt) {
   };
 }
 
-function star(size, x, y) {
-  this.size = size;
-  this.x = x;
-  this.y = y;
-}
-
-class Starfield {
-  constructor(scale, velocityX, velocityY) {
-    this.stars = []; 
-    this.scale = scale;
-    this.velocityX = velocityX;
-    this.velocityY = velocityY;
-    this.totalElapsedMilliseconds = 0;    
-    let starCount = (Math.random() * 100) + 3;  
-    for (let i = 0; i < starCount; ++i) {
-      let radius = scale * (Math.random() * 3) + 1;
-      let x = (Math.random() * canvas.width) + 20;
-      let y = (Math.random() * canvas.height)+ 20;
-      this.stars.push(new star(radius, x, y));
-    }
-  }
- 
-  update(elapsed) {    
-    let elapsedMsSinceLastFrame = elapsed-this.totalElapsedMilliseconds;
-    let xMove = elapsedMsSinceLastFrame * this.velocityX;
-    let yMove = elapsedMsSinceLastFrame * this.velocityY;
-    if (isNaN(xMove) || isNaN(yMove)) return;
-    for (let i = 0; i < this.stars.length; ++i) {
-      
-      // 1. move stars
-      // 2. if the star is out of bounds, move it to ensure it gets displayed again
-      
-      this.stars[i].x += xMove;
-      this.stars[i].y += yMove;      
-      if (this.stars[i].x < 0) {
-        this.stars[i].x = canvas.width;  
-      }
-      if (this.stars[i].x > canvas.width) {
-        this.stars[i].x = 0;
-      }      
-      if (this.stars[i].y < 0) {
-        this.stars[i].y = canvas.height;
-      }      
-      if (this.stars[i].y > canvas.height) {        
-        this.stars[i].y = 0;
-      }
-    }
-    
-    this.totalElapsedMilliseconds = elapsed;
-  };
-  
-  render() {              
-     for(let i = 0; i < this.stars.length; ++i) {
-      ctx.beginPath();
-      ctx.fillStyle = 'white';
-      ctx.arc(this.stars[i].x, this.stars[i].y, this.stars[i].size, 0, 2 * Math.PI, false);
-      ctx.fill();
-    }
-  };
-}
 
 const setup = function() {
-  starfields.push(new Starfield(0.1, 0.01, 0.005));
-  starfields.push(new Starfield(0.6, 0.02, 0.022));
-  starfields.push(new Starfield(1.1, 0.04, 0.028));
+ 
 };
 
 const update = function(elapsed) {    
