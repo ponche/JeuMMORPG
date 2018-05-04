5eec3da94ff4b862727e895ec7adf6ae3fc639ad
Rémi <ponche62880@gmail.com>
2018-05-04 13:27:34 +0200

    premier test

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="assets/css/custom.css" />
+	<title>Jeu MMORPG</title>
+</head>
+
+<body>
+	<h1>Page de test</h1>
+	<canvas id="canvas">Navigateur obsolete</canvas>
+	
+	<script src="assets/js/custom.js" />
+	
+</body>
+
+
+
+</html>
