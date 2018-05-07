453f5d64ed077c920502d510ad87bb2ab1788838
Rémi <ponche62880@gmail.com>
2018-05-07 11:58:07 +0200

    add class BehaviorComposant

---
+++ assets/js/class/BehaviorComposant.js
@@ -0,0 +1,22 @@
+// Class Abtraire, permet de faire les composant personnalité (script)  des Actor en dérivant de cette class. 
+
+class BehaviorComposant 
+{
+	constructor(actor)
+	{
+		// Recupere les information de l'actor pour pouvoir les manipuler
+		this.actor = actor ; 
+	}
+	update()
+	{
+	}
+	collision(actorCollision)
+	{
+	}
+	clickMouse()
+	{
+	}
+	
+}
+
+	
